57e3820b8fbc3dc4efa8337d50c00db378daf0d0
cousinbigcountry <fountainkylec@gmail.com>
2025-07-23 18:27:00 -0400

    Connected backend data to display on frontend and built basic frontend

--- backend/ecommerce/src/main/java/com/lft/ecommerce/dao/ProductCategoryRepository.java
+++ backend/ecommerce/src/main/java/com/lft/ecommerce/dao/ProductCategoryRepository.java
@@ -2,9 +2,10 @@ package com.lft.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.lft.ecommerce.entity.ProductCategory;
-
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path="product-categories")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long>{
 
--- backend/ecommerce/src/main/java/com/lft/ecommerce/entity/Product.java
+++ backend/ecommerce/src/main/java/com/lft/ecommerce/entity/Product.java
@@ -40,7 +40,7 @@ public class Product {
     private String description;
 
     @Column(name="unit_price")
-    private BigDecimal unitprice;
+    private BigDecimal unitPrice;
 
     @Column(name="image_url")
     private String imageUrl;
